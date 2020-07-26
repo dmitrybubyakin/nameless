0137b66a0c49789bb1da98e0d1ea3525ccd5843b
kshitij.singh <kshitij.singh@go-jek.com>
2020-07-26 14:21:51 +0530

    Update PSQL configs

--- src/nameless/config.clj
+++ src/nameless/config.clj
@@ -7,7 +7,7 @@
 
 (defn db-jdbc-uri []
   (let [{:keys [type server port name user password]} (:db env)
-        uri (format "jdbc:%s://%s:%s/%s/cloudsql/namelss:us-central1:namelss-db?user=%s&password=%s"
+        uri (format "jdbc:%s://%s:%s/%s?user=%s&password=%s"
                     type server port name user password)]
     (log/info "Starting db Connection with uri" uri)
     uri))
